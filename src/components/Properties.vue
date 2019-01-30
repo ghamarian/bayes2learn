4ba0ad51cb34b96a7ffd231d6d626d3f9594c2a4
Amir <amirhossein.gh@gmail.com>
2019-01-30 23:22:53 +0100

    Addign properties component

---
+++ src/components/Properties.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="main-bar">
+    <h1>{{ properties }}</h1>
+  </div>
+</template>
+
+<script>
+import distributions from "../assets/distributions.json";
+
+export default {
+  name: "Amir",
+  computed: {
+    properties() {
+      return Object.keys(distributions.Distributions[this.$store.state.selected_node]);
+    }
+  },
+  data: function() {
+    return {
+      distributions: distributions.Distributions
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-bar {
+  border: 1px solid pink;
+  overflow: scroll;
+  height: calc(100vh - 110px);
+  box-sizing: border-box;
+  text-align: center;
+}
+
+.main-button-list {
+  display: grid;
+  grid-template-columns: minmax(200px, 1fr);
+  grid-gap: 10px;
+  overflow: scroll;
+  text-align: center;
+}
+
+.button-list {
+  display: grid;
+  grid-template-columns: minmax(200px, 1fr);
+  grid-gap: 2px;
+  /* height: 100vh; */
+  overflow: scroll;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
