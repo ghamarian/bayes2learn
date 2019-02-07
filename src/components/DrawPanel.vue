65fe828661e3d71c1126c9c007474f2a0744d431
Amir <amirhossein.gh@gmail.com>
2019-02-07 22:52:44 +0100

    converted to vanilla version again

--- src/components/DrawPanel.vue
+++ src/components/DrawPanel.vue
@@ -1,203 +1,206 @@
 <template>
-  <cytoscape :config="config" :preConfig="preConfig" :afterCreated="afterCreated"/>
+  <div id="cyto" ref="cyto"></div>
 </template>
 <script>
-import jquery from "jquery";
+import cytoscape from "cytoscape";
+import { mapMutations, mapGetters } from "vuex";
 import contextMenus from "cytoscape-context-menus";
 import "cytoscape-context-menus/cytoscape-context-menus.css";
-import { mapMutations, mapGetters } from 'vuex';
-
-let selected_color = "#666666";
-let white = "#ffffff";
-const config = {
-  elements: [
-    {
-      // node a
-      data: {
-        id: "Autoregressive",
-        content: "Autoregressive",
-        weight: 75,
-        name: "Autoregressive"
-      }
-    },
-    {
-      // node b
-      data: {
-        id: "BatchReshape",
-        content: "BatchReshape",
-        name: "BatchReshape"
-      }
-    },
-    {
-      // edge ab
-      data: { id: "ab", source: "Autoregressive", target: "BatchReshape" }
-    }
-  ],
-  style: [
-    // the stylesheet for the graph
-    {
-      selector: "node",
-      style: {
-        "text-valign": "center",
-        color: "white",
-        width: 200,
-        height: 40,
-        shape: "roundrectangle",
-        label: `data(name)`
-      }
-    },
-    // {
-    //     selector: 'node:selected',
-    //     style: {
-    //         'border-width': 3,
-    //         'border-color': selected_color
-    //
-    //     }
-    // },
-
-    {
-      selector: "edge",
-      style: {
-        width: 3,
-        "line-color": "#ccc",
-        "target-arrow-color": "#ccc",
-        "target-arrow-shape": "triangle"
-      }
-    },
-    {
-      selector: ".eh-handle",
-      style: {
-        "background-color": selected_color,
-        width: 12,
-        height: 12,
-        shape: "ellipse",
-        "overlay-opacity": 0,
-        "border-width": 12, // makes the handle easier to hit
-        "border-opacity": 0
-      }
-    },
-    {
-      selector: ".eh-hover",
-      style: {
-        "background-color": selected_color
-      }
-    },
-    {
-      selector: ".eh-source",
-      style: {
-        "border-width": 2,
-        "border-color": selected_color
-      }
-    },
-    {
-      selector: ".eh-target",
-      style: {
-        "border-width": 2,
-        "border-color": selected_color
-      }
-    },
-    {
-      selector: ".eh-preview, .eh-ghost-edge",
-      style: {
-        "background-color": selected_color,
-        "line-color": selected_color,
-        "target-arrow-color": selected_color,
-        "source-arrow-color": selected_color
-      }
-    },
-    {
-      selector: ".eh-ghost-edge .eh-preview-active",
-      style: {
-        opacity: 0
-      }
-    }
-  ],
-  layout: {
-    name: "grid",
-    rows: 1
-  }
-};
+import jquery from "jquery";
 
 export default {
-  name: "DrawPanel",
+  name: "HelloWorld",
   data: function() {
     return {
-      config: config
+      a: 2
     };
-  },
-  computed: {
-    ...mapGetters(["getNewNode"]),
   },
   props: {
     msg: String
   },
+  computed: {
+    ...mapGetters(["getNewNode"])
+  },
   methods: {
-    ...mapMutations(["selectNode"]),
-    preConfig(cytoscape) {
-      // it can be used both ways
-      contextMenus(cytoscape, jquery);
-      // cytoscape.use(contextMenus, jquery)
-    },
-    afterCreated(cy) {
-      const that = this;
-
-      cy.on("tap", event => {
-        let evtTarget = event.target;
-        if (evtTarget === cy) {
-          let new_node = cy.add({
-            group: "nodes",
-            data: {
-              name: that.getNewNode.name,
-              root: "",
-              weight: 75,
-              content: that.getNewNode.properties 
-            },
-            position: event.position
-          });
-        }
-      });
-      cy.on("tap", "node", function(evt) {
-        console.log(`${evt.target.id()}, ${evt.target.data().content}`);
-        that.selectNode(evt.target.data('name'));
-      });
-      cy.contextMenus({
-        menuItems: [
-          {
-            id: "remove",
-            content: "remove",
-            tooltipText: "remove",
-            image: { src: "remove.svg", width: 12, height: 12, x: 6, y: 4 },
-            selector: "node, edge",
-            onClickFunction: function(event) {
-              var target = event.target || event.cyTarget;
-              target.remove();
-            },
-            hasTrailingDivider: true
-          },
-          {
-            id: "hide",
-            content: "hide",
-            tooltipText: "hide",
-            selector: "*",
-            onClickFunction: function(event) {
-              var target = event.target || event.cyTarget;
-              target.hide();
-            },
-            disabled: false
+    ...mapMutations(["selectNode"])
+  },
+  mounted() {
+    this.cy = cytoscape({
+      container: this.$refs.cyto,
+      elements: [
+        // list of graph elements to start with
+        {
+          // node a
+          data: {
+            id: "Autoregressive",
+            content: "Autoregressive",
+            weight: 75,
+            name: "Autoregressive",
+            parent: ["a", "b"]
           }
-        ]
-      });
-    }
+        },
+        {
+          // node b
+          data: {
+            id: "BatchReshape",
+            content: "BatchReshape",
+            name: "BatchReshape",
+            parent: "b"
+          }
+        },
+        {
+          // node b
+          data: {
+            id: "BatchReshape",
+            content: "BatchReshape",
+            name: "BatchReshape",
+            parent: ["b", "d"]
+          }
+        },
+        {
+          // node b
+          data: { id: "a", name: "a" }
+        },
+        {
+          // node b
+          data: { id: "b", name: "b" }
+        },
+        {
+          // node b
+          data: { id: "d", name: "c" }
+        },
+        {
+          // edge ab
+          data: { id: "ab", source: "a", target: "b" }
+        }
+      ],
+
+      style: [
+        // the stylesheet for the graph
+
+        {
+          selector: "edge",
+          style: {
+            width: 3,
+            "line-color": "#ccc",
+            "target-arrow-color": "#ccc",
+            css: {
+              "curve-style": "bezier",
+              "target-arrow-shape": "triangle"
+            }
+          }
+        },
+        {
+          selector: "node",
+          css: {
+            content: "data(id)",
+            "text-valign": "center",
+            "text-halign": "center",
+            shape: "roundrectangle",
+            label: `data(name)`
+          }
+        },
+        {
+          selector: "$node > node",
+          css: {
+            "padding-top": "10px",
+            "padding-left": "10px",
+            "padding-bottom": "10px",
+            "padding-right": "10px",
+            "text-valign": "top",
+            "text-halign": "center",
+            "background-color": "#ffffff",
+            opacity: 0.5
+          }
+        },
+        {
+          selector: ":selected",
+          css: {
+            "background-color": "black",
+            "line-color": "black",
+            "target-arrow-color": "black",
+            "source-arrow-color": "black"
+          }
+        }
+      ],
+
+      layout: {
+        name: "grid",
+        rows: 1
+      }
+    });
+
+    let vm = this;
+    // this.cy.on("tap", function(event) {
+    //   let evtTarget = event.target;
+    //   if (evtTarget === vm.cy) {
+    //     let new_node = vm.cy.add({
+    //       group: "nodes",
+    //       data: {
+    //         name: "a",
+    //         root: "a",
+    //         weight: 75,
+    //         content: "a"
+    //       },
+    //       position: event.position
+    //     });
+    //   }
+    // });
+
+    this.cy.on("tap", event => {
+      let evtTarget = event.target;
+      if (evtTarget === vm.cy) {
+        let new_node = vm.cy.add({
+          group: "nodes",
+          data: {
+            name: vm.getNewNode.name,
+            root: "",
+            weight: 75,
+            content: vm.getNewNode.properties
+          },
+          position: event.position
+        });
+      }
+    });
+    this.cy.on("tap", "node", function(evt) {
+      console.log(`${evt.target.id()}, ${evt.target.data().content}`);
+      vm.selectNode(evt.target.data("name"));
+    });
+    contextMenus(cytoscape, jquery);
+
+    this.cy.contextMenus({
+      menuItems: [
+        {
+          id: "remove",
+          content: "remove",
+          tooltipText: "remove",
+          image: { src: "remove.svg", width: 12, height: 12, x: 6, y: 4 },
+          selector: "node, edge",
+          onClickFunction: function(event) {
+            var target = event.target || event.cyTarget;
+            target.remove();
+          },
+          hasTrailingDivider: true
+        },
+        {
+          id: "hide",
+          content: "hide",
+          tooltipText: "hide",
+          selector: "*",
+          onClickFunction: function(event) {
+            var target = event.target || event.cyTarget;
+            target.hide();
+          },
+          disabled: false
+        }
+      ]
+    });
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-cytoscape {
-  height: 100%;
-  background: mistyrose;
-}
 #cyto {
   height: 100%;
   display: block;
--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import BootstrapVue from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueCytoscape from "vue-cytoscape";
-import "vue-cytoscape/dist/vue-cytoscape.css";
+import "cytoscape";
+// import VueCytoscape from "vue-cytoscape";
+// import "vue-cytoscape/dist/vue-cytoscape.css";
 
-Vue.use(VueCytoscape);
+// Vue.use(VueCytoscape);
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
