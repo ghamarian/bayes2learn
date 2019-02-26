0f9ea376ee104912d4338406a8f6bd744925823c
Amir Ghamarian <amir@machine2learn.nl>
2019-02-26 15:56:41 +0100

    Moving to vuetify in incomingproperty, still edit and delete must be implemented.

--- src/components/IncomingProperty.vue
+++ src/components/IncomingProperty.vue
@@ -1,36 +1,72 @@
 <template>
-  <b-modal
-    v-model="toShow"
-    no-close-on-esc
-    no-close-on-backdrop
-    @ok="closeModal"
-    @cancel="closeModal"
-  >
-    <b-list-group>
-      <b-list-group-item
-        v-for="item in properties"
-        :key="item"
-        class="d-flex justify-content-between align-items-center"
-        button
-        @click="click"
-        :class="{ 'active': isSelected(item), 'disabled': false}"
-      >{{ item }}</b-list-group-item>
-    </b-list-group>
-  </b-modal>
+  <div v-if="toShow">
+    <v-layout row justify-center>
+      <v-dialog v-model="toShow" persistent max-width="600px">
+        <v-card>
+          <v-card-title>
+            <span class="headline">Incoming property</span>
+          </v-card-title>
+          <v-card-text>
+            <v-data-table
+              :headers="headers"
+              :items="propertiesObjects"
+              class="elevation-1"
+              v-model="selected"
+              item-key="name"
+            >
+              <template slot="items" slot-scope="props">
+                <tr @click="select(props.item)" :active="props.selected">
+                  <td>{{ props.item.name }}</td>
+                  <td class="justify-center layout px-0">
+                    <v-icon small class="mr-2" @click="editItem(props.item.name)">edit</v-icon>
+                    <v-icon small @click="deleteItem(props.item)">delete</v-icon>
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="closeModal">Close</v-btn>
+            <v-btn color="blue darken-1" flat @click="closeModal">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </div>
 </template>
 
 <script>
 import { mapMutations, mapGetters } from "vuex";
 
 export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Name",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Actions", value: "name", sortable: false, align: "center" }
+      ]
+    };
+  },
   props: ["modalShow", "properties"],
   computed: {
     ...mapGetters(["getEdgeIncomingProperty"]),
+    selected(){
+      return [{ name: this.getEdgeIncomingProperty }]
+    },
     toShow: {
       get() {
         return this.modalShow;
       },
       set() {}
+    },
+    propertiesObjects() {
+      return this.properties.map(ele => Object.assign({}, { name: ele }));
     }
   },
   methods: {
@@ -41,9 +77,45 @@ export default {
     click(event) {
       this.updateEdgeValue(event.target.innerText);
     },
-    isSelected(item) {
-      return item === this.getEdgeIncomingProperty;
+    select(selectedItem) {
+      this.propertiesObjects.forEach(item => {
+        if (item.name == selectedItem.name) {
+          this.updateEdgeValue(selectedItem.name);
+        }
+      });
+    },
+    editItem(item) {
+      this.editedIndex = this.properties.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.properties.splice(index, 1);
+    },
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
     }
   }
 };
 </script>
+
+<style scoped>
+tr[active^="true"] {
+  background-color: darkgray !important;
+}
+</style>
+
